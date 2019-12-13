3af10285b7d48db46b1e35a9d7f77da6a844c307
Mark Müller <markmueller86@gmail.com>
2019-12-13 20:46:52 +0100

    squeekboard-test-layout: correct dependencies and version for clap argument parsing

--- src/bin/test_layout.rs
+++ src/bin/test_layout.rs
@@ -7,7 +7,6 @@ use rs::tests::check_layout_file;
 fn main() -> () {
     let matches = clap_app!(test_layout =>
         (name: "squeekboard-test-layout")
-        (version: env!("CARGO_PKG_VERSION"))
         (about: "Test keyboard layout for errors. Returns OK or an error message containing further information.")
         (@arg INPUT: +required "Yaml keyboard layout file to test")
     ).get_matches();
